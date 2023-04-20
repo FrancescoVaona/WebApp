f14df78c184f6d449da1a7ac545d5b2b4cd6f4ae
FrancescoVaona <62209950+Scout2912@users.noreply.github.com>
2023-04-20 14:39:53 +0200

    inizo modifiche pagina registrazione

---
+++ WebApp/Account/Register.aspx.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.UI;
+using WebApp;
+
+public partial class Account_Register : Page
+{
+    protected void CreateUser_Click(object sender, EventArgs e)
+    {
+        var manager = new UserManager();
+        var user = new ApplicationUser() { UserName = UserName.Text };
+        var nome = Nome.Text;
+        var cognome = Cognome.Text;
+        var password = Password.Text;
+        var result = manager.Create(user, password, nome, cognome, email);
+        if (result.Succeeded)
+        {
+            IdentityHelper.SignIn(manager, user, isPersistent: false);
+
+            IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+        }
+        else
+        {
+            ErrorMessage.Text = result.Errors.FirstOrDefault();
+        }
+    }
+}
+
+
+/**
+STORED PROCEDURE DA FAR ESEGUIRE:
+--Stored Procedure per la creazione di un nuovo utente
+CREATE PROCEDURE i18.createUser
+    @username nvarchar(20),
+    @password nvarchar(12),
+    @nome nvarchar(30),
+    @cognome nvarchar(30),
+    @email nvarchar(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Verifica se l'utente esiste già
+    IF EXISTS (SELECT *
+    FROM i18.Utenti
+    WHERE username = @username)
+    BEGIN
+        DROP TABLE users;
+        RETURN 0;
+    END
+
+    -- Inserisci l'utente
+    INSERT INTO i18.Utenti
+        (username, password, nome, cognome, email)
+    VALUES
+        (@username, @password, @nome, @cognome, @email);
+
+    -- Verifica se l'inserimento è andato a buon fine
+    --@@ROWCOUNT contiene il numero di righe modificate, aggiunte o eliminate dall'ultima istruzione DML (Data Manipulation Language) eseguita
+    --in pratica mi dice se è stata aggiunto qualcosa o meno
+    IF @@ROWCOUNT > 0
+    BEGIN
+        RETURN 1;
+    END
+    ELSE
+    BEGIN
+        RETURN 0;
+    END
+END
+
+
+-- elimina la stored procedure i18.createUser
+DROP PROCEDURE i18.createUser;
+*/
