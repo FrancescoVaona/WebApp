6abcf0776886eaec7f5122bae5c45c6788aef8da
FrancescoVaona <62209950+Scout2912@users.noreply.github.com>
2023-05-24 13:32:47 +0200

    pagina privata V2, ora completa

--- WebApp/Account/Private.aspx.cs
+++ WebApp/Account/Private.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,5 +10,14 @@ public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        String user = (String)HttpContext.Current.Session["UserLogged"];
+        if (user != null)
+        {
+            // Fai qualcosa con user (è una stringa)
+        }
+        else
+        {
+            Response.Redirect("/");
+        }
     }
 }
